3cafdea8438c9c0f1b0fc9167fdfbe6096d5b901
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-09-09 13:13:40 +0530

    Deleted

--- src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-   return (
-      <div className="flex items-center justify-center">
-         <h1 className="font-extrabold text-3xl md:text-5xl text-white">IP Address Tracker</h1>
-      </div>
-   )
-}
